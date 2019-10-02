2ecf5efcc46776e1db67d6b22d3de52fe8479e2f
Max Dekker <xamrekked@gmail.com>
2019-10-02 10:01:35 +0200

    Enter ID update

---
+++ TestLocationPlacements/Assets/Scripts/EnterID.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterID : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text currentID;
+
+    public void AddID(int number)
+    {
+        if(currentID.text == "0")
+        {
+            currentID.text = number.ToString();
+        }
+        else
+        {
+            currentID.text += number;
+        }
+    }
+
+    public void RemoveID()
+    {
+        string laststring = currentID.text.Remove(currentID.text.Length - 1, 1);
+
+        currentID.text = laststring;
+
+        if (laststring == "") 
+        {
+            currentID.text = "0";
+        }
+    }
+
+
+
+}
